d62caf845105085acc4a8852284079356699e39b
juanalbertogutierrez <juanalberto.gtzc@gmail.com>
2018-08-19 20:51:53 -0500

    version final

---
+++ motor_prueba1/motor_prueba1.ino
@@ -0,0 +1,44 @@
+int PinIN1 = 7;
+int PinIN2 = 6;
+
+void setup() {
+  // inicializar la comunicación serial a 9600 bits por segundo:
+  Serial.begin(9600);
+  // configuramos los pines como salida
+  pinMode(PinIN1, OUTPUT);
+  pinMode(PinIN2, OUTPUT);
+}
+
+void loop() {
+  
+  MotorHorario();
+  Serial.println("Giro del Motor en sentido horario");
+  delay(1000);
+  
+  MotorAntihorario();
+  Serial.println("Giro del Motor en sentido antihorario");
+  delay(1000);
+  
+  MotorStop();
+  Serial.println("Motor Detenido");
+  delay(1000);
+  
+}
+
+void MotorHorario()
+{
+  digitalWrite (PinIN1, HIGH);
+  digitalWrite (PinIN2, LOW);
+}
+void MotorAntihorario()
+{
+  digitalWrite (PinIN1, LOW);
+  digitalWrite (PinIN2, HIGH);
+}
+
+void MotorStop()
+{
+  digitalWrite (PinIN1, LOW);
+  digitalWrite (PinIN2, LOW);
+}
+
